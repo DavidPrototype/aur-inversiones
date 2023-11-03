c8b6320f2e6a96b43910b95ec58af7248bb00bc1
David Sánchez Cortez <dsanchez@prototype.mx>
2023-11-02 23:56:48 -0600

    feat:asesoria pagina

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -5,8 +5,9 @@ export default class Header extends HTMLElement {
 
     connectedCallback() {
         let active = this.getAttribute("nav-active");
-
-        this.innerHTML = `
+        let mostrarMenu = this.getAttribute("mostrar-menu");
+       
+        let htmlContent= `
         <header class="header">
             <nav class="nav">
                 <div class="nav__logo">
@@ -18,19 +19,27 @@ export default class Header extends HTMLElement {
                             <img src="../img/Logotipo.svg" alt="Logo BanCoppel" />
                         </a>
                     </div>
-                </div>
-                <div class="nav--menu custom-container">
-                    <ul class="menu font-secondary">
-                        <li class="menu__item" id="nav-inicio"><a href="index.html">Inicio</a></li>
-                        <li class="menu__item" id="nav-inversion"><a href="inversion-creciente.html">Inversión Creciente</a></li>
-                        <li class="menu__item" id="nav-pagare"><a href="pagare.html">Pagaré</a></li>
-                        
-                    </ul>
-                </div>
-                    
-                </nav>
+                </div>`;
+       
+
+                if(mostrarMenu == "show"){
+                    htmlContent += `
+                    <div class="nav--menu custom-container">
+                        <ul class="menu font-secondary">
+                            <li class="menu__item" id="nav-inicio"><a href="index.html">Inicio</a></li>
+                            <li class="menu__item" id="nav-inversion"><a href="inversion-creciente.html">Inversión Creciente</a></li>
+                            <li class="menu__item" id="nav-pagare"><a href="pagare.html">Pagaré</a></li>
+                            
+                        </ul>
+                    </div>
+                    `;
+                }
+
+                htmlContent += `            
+            </nav>
         </header>
         `;
+        this.innerHTML =htmlContent;
 
         this.getAttribute("nav-active")
             ? document
