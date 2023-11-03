ee47e19ea8aafed7e434bfe4833d70c8c2879246
David Sánchez Cortez <dsanchez@prototype.mx>
2023-11-03 01:47:06 -0600

    fix: header ocultar/mostrar en oag asesoria

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -5,9 +5,9 @@ export default class Header extends HTMLElement {
 
     connectedCallback() {
         let active = this.getAttribute("nav-active");
-        let mostrarMenu = this.getAttribute("mostrar-menu");
+        // let mostrarMenu = this.getAttribute("mostrar-menu");
        
-        let htmlContent= `
+        this.innerHTML= `
         <header class="header">
             <nav class="nav">
                 <div class="nav__logo">
@@ -19,27 +19,23 @@ export default class Header extends HTMLElement {
                             <img src="../img/Logotipo.svg" alt="Logo BanCoppel" />
                         </a>
                     </div>
-                </div>`;
-       
-
-                if(mostrarMenu == "show"){
-                    htmlContent += `
-                    <div class="nav--menu custom-container">
+                </div>
+                
+                <div class="nav--menu custom-container">
                         <ul class="menu font-secondary">
                             <li class="menu__item" id="nav-inicio"><a href="index.html">Inicio</a></li>
                             <li class="menu__item" id="nav-inversion"><a href="inversion-creciente.html">Inversión Creciente</a></li>
                             <li class="menu__item" id="nav-pagare"><a href="pagare.html">Pagaré</a></li>
                             
                         </ul>
-                    </div>
-                    `;
-                }
-
-                htmlContent += `            
+                </div>
             </nav>
         </header>
-        `;
-        this.innerHTML =htmlContent;
+                `;
+       
+
+
+      
 
         this.getAttribute("nav-active")
             ? document
