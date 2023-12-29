b33595de150c6ce546d57888cf70084c66e8e6e6
David Sánchez Cortez <dsanchez@prototype.mx>
2023-12-29 17:24:39 -0600

    fix: dist

---
+++ dist/js/envio-aa0ec907.js
@@ -0,0 +1,1 @@
+import"./main-efff4ac1.js";dataLayer.push({page:"/inversiones/listo-para-invertir/gracias",title:"Inversiones - Listo para invertir - Gracias",event:"pvGeneral"});document.getElementById("btn-envio").addEventListener("click",()=>{dataLayer.push({nd1:"Inversiones - Listo para invertir -  Gracias",nd2:"Volver al inicio",nd3:"",nd4:"",event:"inversiones"})});
